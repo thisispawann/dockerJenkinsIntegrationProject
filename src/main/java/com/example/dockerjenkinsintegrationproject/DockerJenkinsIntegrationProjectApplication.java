6cc906b760fb5ecc3995e195724afec4e7d9bdc9
Pawan Kumar Shrestha <thisispawann@gmail.com>
2024-12-26 21:25:52 +0545

    first commit

---
+++ src/main/java/com/example/dockerjenkinsintegrationproject/DockerJenkinsIntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintegrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkinsIntegrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerjenkinsintegrationproject/DockerJenkinsIntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinsintegrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
